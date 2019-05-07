3e648dac1a17e73937f30bc2ce5fad7c0d91abf3
sunstar <243280864@qq.com>
2019-05-07 09:55:54 +0800

    Initial commit

---
+++ lib/ss_flutter_view_support.dart
@@ -0,0 +1,56 @@
+library ss_flutter_view_support;
+
+import 'package:flutter/material.dart';
+
+class LoadingView extends StatefulWidget {
+  Widget child;
+  Widget loadingView;
+  _LoadingView innerView;
+
+  LoadingView({@required this.child, this.loadingView});
+
+  void loadingComplete() {}
+
+  @override
+  State<StatefulWidget> createState() {
+    if (innerView == null) {
+      innerView = _LoadingView(child, loadingView);
+    }
+    return innerView;
+  }
+}
+
+class _LoadingView extends State<LoadingView> {
+  Widget child;
+  Widget loadingView;
+
+  _LoadingView(this.child, this.loadingView);
+
+  bool _isLoading = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: _isLoading ? loadingView : child,
+      ),
+    );
+  }
+
+  void loadingComplete() {
+    setState(() {
+      _isLoading = false;
+    });
+  }
+}
+
+class LoadingHelp {
+  static Widget _loadingView = Container();
+
+  static Widget get loadingView => _loadingView;
+
+  LoadingHelp initLoadingBaseView(Widget view) {
+    _loadingView = view;
+    return this;
+  }
+}
---
+++ test/ss_flutter_view_support_test.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:ss_flutter_view_support/ss_flutter_view_support.dart';
+
+void main() {
+}
